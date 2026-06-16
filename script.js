71583bbb0f04edcd8265c3a4a7386ec1da2fe22f
Gokulkannan-G-dev <gokulkannan18v@gmail.com>
2026-06-16 13:11:08 +0530

    modified

--- script.js
+++ script.js
@@ -1,31 +1,42 @@
 let led = [];
+
 let inputid = document.getElementById("input-id");
 let inputbtn = document.getElementById("button-save");
 let ul = document.getElementById("ul-li");
 
 
-inputbtn.addEventListener("click" , function(){
-    led.push(inputid.value);
-    renderItems();
-    inputid.value = "";
-});
+
+   inputbtn.addEventListener("click", function () {
+
+      let value = inputid.value;
+  
+      led.push(value);
+  
+      localStorage.setItem("led", JSON.stringify(led));
+  
+      renderItems();
+  
+      inputid.value = "";
+      console.log(localStorage.getItem("led"));
+  
+  });
 
 
-function renderItems(){
+
+  function renderItems() {
+
    let listitem = "";
-   listitem += `
-   <li>
-      <a target='_blank' href='${inputid.value}'>${inputid.value}</a>
-   </li>
-   
-   `;
 
-   ul.innerHTML += listitem
-  //let a = document.createElement("li");
-  //a.innerHTML = inputid.value;
-   //ul.append(a);
+   for (let i = 0; i < led.length; i++) {
 
+       listitem += `
+       <li>
+           <a target="_blank" href="${led[i]}">${led[i]}</a>
+       </li>
+       `;
+   }
 
+   ul.innerHTML = listitem;
 }
 
 
